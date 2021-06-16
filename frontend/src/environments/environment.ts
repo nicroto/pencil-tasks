8f82380631b74f5d152dd0b2483e64bb5f8947b8
Nikolay Tsenkov <nikolay@tsenkov.net>
2021-06-16 19:42:57 +0300

    Add: frontend: firebase config.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDpr3uzeHDbBLZZfQdG_avsHwwH77iPRp8",
+    authDomain: "pencil-frontend-15eef.firebaseapp.com",
+    projectId: "pencil-frontend-15eef",
+    storageBucket: "pencil-frontend-15eef.appspot.com",
+    messagingSenderId: "868072610474",
+    appId: "1:868072610474:web:8d8290a4bd861575af4ed3"
+  }
 };
 
 /*
