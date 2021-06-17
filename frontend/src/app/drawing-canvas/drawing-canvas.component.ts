61c06fa23c4566d5afb95b791efcbe73e5ca33b2
Nikolay Tsenkov <nikolay@tsenkov.net>
2021-06-18 00:04:26 +0300

    Add: frontend: drawing canvas to the home page.
    Add: frontend: fabric canvas to the drawing canvas.

--- frontend/src/app/drawing-canvas/drawing-canvas.component.ts
+++ frontend/src/app/drawing-canvas/drawing-canvas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-drawing-canvas',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrawingCanvasComponent implements OnInit {
 
-  constructor() { }
+  private _canvas?: fabric.Canvas;
+  private _mouseDown: (evt: fabric.IEvent) => void;
+  private _mouseMove: (evt: fabric.IEvent) => void;
+  private _mouseUp: (evt: fabric.IEvent) => void;
+
+  constructor(private zone: NgZone) {
+    this._mouseDown = (evt: fabric.IEvent) => this.__onMouseDown(evt);
+    this._mouseMove = (evt: fabric.IEvent) => this.__onMouseMove(evt);
+    this._mouseUp = (evt: fabric.IEvent) => this.__onMouseUp(evt);
+  }
 
   ngOnInit(): void {
+    this.zone.runOutsideAngular(() => {
+      this._canvas = new fabric.Canvas('canvasElement', {
+        backgroundColor: '#fbfbfd',
+        selection: false,
+        preserveObjectStacking: true
+      });
+
+      this._canvas.on('mouse:down', this._mouseDown);
+      this._canvas.on('mouse:move', this._mouseMove);
+      this._canvas.on('mouse:up', this._mouseUp);
+    });
+  }
+
+  private __onMouseDown(evt: fabric.IEvent): void {
+  }
+  private __onMouseMove(evt: fabric.IEvent): void {
+  }
+  private __onMouseUp(evt: fabric.IEvent): void {
   }
 
 }
