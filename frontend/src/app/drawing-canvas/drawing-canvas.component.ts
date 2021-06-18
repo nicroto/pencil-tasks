48bc740ce69cc383fa3e0f476869dd101d48c396
Nikolay Tsenkov <nikolay@tsenkov.net>
2021-06-18 03:55:58 +0300

    Clean: frontend: drawing canvas: unnecessary event handlers.

--- frontend/src/app/drawing-canvas/drawing-canvas.component.ts
+++ frontend/src/app/drawing-canvas/drawing-canvas.component.ts
@@ -11,19 +11,13 @@ import { DrawingService } from '../shared/services/drawing.service';
 export class DrawingCanvasComponent implements OnInit {
 
   private _canvas?: fabric.Canvas;
-  private _mouseDown: (evt: fabric.IEvent) => void;
-  private _mouseMove: (evt: fabric.IEvent) => void;
-  private _mouseUp: (evt: fabric.IEvent) => void;
+
   private colorChangeSubscription: Subscription;
 
   constructor(
     private zone: NgZone,
     private drawing: DrawingService
   ) {
-    this._mouseDown = (evt: fabric.IEvent) => this.__onMouseDown(evt);
-    this._mouseMove = (evt: fabric.IEvent) => this.__onMouseMove(evt);
-    this._mouseUp = (evt: fabric.IEvent) => this.__onMouseUp(evt);
-
     this.colorChangeSubscription = this.drawing.color.subscribe(value => {
       if (this._canvas) {
         this._canvas.freeDrawingBrush.color = `#${value.toHex()}`;
@@ -43,23 +37,12 @@ export class DrawingCanvasComponent implements OnInit {
       this._canvas.freeDrawingBrush = brush;
       this._canvas.isDrawingMode = true;
 
-      this._canvas.on('mouse:down', this._mouseDown);
-      this._canvas.on('mouse:move', this._mouseMove);
-      this._canvas.on('mouse:up', this._mouseUp);
-
       this.drawing.color.next(new fabric.Color("#000000"));
     });
   }
 
   ngOnDestroy(): void {
     this.colorChangeSubscription.unsubscribe();
-  }
-
-  private __onMouseDown(evt: fabric.IEvent): void {
-  }
-  private __onMouseMove(evt: fabric.IEvent): void {
-  }
-  private __onMouseUp(evt: fabric.IEvent): void {
   }
 
 }
