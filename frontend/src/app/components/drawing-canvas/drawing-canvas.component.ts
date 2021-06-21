6846c8060707e87c5f4c38accc2b83a36f780f7f
Nikolay Tsenkov <nikolay@tsenkov.net>
2021-06-21 23:48:53 +0300

    Refactor: frontend: DrawingCanvas: extract constants.

--- frontend/src/app/components/drawing-canvas/drawing-canvas.component.ts
+++ frontend/src/app/components/drawing-canvas/drawing-canvas.component.ts
@@ -4,6 +4,20 @@ import { fabric } from 'fabric';
 import { DrawingService } from '../../shared/services/drawing.service';
 import { Document } from '../../shared/types/document.class';
 
+const CANVAS_SETTINGS = {
+  BASE: {
+    backgroundColor: '#fbfbfd',
+    selection: false,
+    preserveObjectStacking: true
+  },
+  DRAWING_COLOR: "#000000",
+  EVENTS: {
+    OBJECT_ADDED: 'object:added',
+    OBJECT_MODIFIED: 'object:modified'
+  }
+};
+
+
 @Component({
   selector: 'app-drawing-canvas',
   templateUrl: './drawing-canvas.component.html',
@@ -39,20 +53,16 @@ export class DrawingCanvasComponent implements OnInit {
 
   ngOnInit(): void {
     this.zone.runOutsideAngular(() => {
-      this._canvas = new fabric.Canvas('canvasElement', {
-        backgroundColor: '#fbfbfd',
-        selection: false,
-        preserveObjectStacking: true
-      });
+      this._canvas = new fabric.Canvas('canvasElement', CANVAS_SETTINGS.BASE);
 
       let brush = new fabric.PencilBrush(this._canvas);
       this._canvas.freeDrawingBrush = brush;
       this._canvas.isDrawingMode = true;
 
-      this._canvas.on('object:added', this._objectAdded);
-      this._canvas.on('object:modified', this._objectModified);
+      this._canvas.on(CANVAS_SETTINGS.EVENTS.OBJECT_ADDED, this._objectAdded);
+      this._canvas.on(CANVAS_SETTINGS.EVENTS.OBJECT_MODIFIED, this._objectModified);
 
-      this.drawing.color.next(new fabric.Color("#000000"));
+      this.drawing.color.next(new fabric.Color(CANVAS_SETTINGS.DRAWING_COLOR));
 
       this.loadDataFromDocument();
     });
